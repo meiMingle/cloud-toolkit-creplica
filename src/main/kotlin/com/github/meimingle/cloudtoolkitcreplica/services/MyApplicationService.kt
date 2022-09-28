df7661606d5505ee66caa0ab3e4bba7f1a16bd08
GitHub Action <action@github.com>
2022-09-28 14:32:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/meimingle/cloudtoolkitcreplica/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/meimingle/cloudtoolkitcreplica/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.meimingle.cloudtoolkitcreplica.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meimingle.cloudtoolkitcreplica.MyBundle
 
 class MyApplicationService {
 
